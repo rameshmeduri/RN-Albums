9f7004668d80df9d85b48ac4df365d670db94df8
Ramesh Meduri <m.ramesh@enterpi.com>
2019-05-29 15:37:01 +0530

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,5 @@
-// Import libraries for making a component
 import React from 'react';
 import { Text, View } from 'react-native';
-
-// Make a component
-const Header = (props) => {
-  const { textStyle, viewStyle } = styles;
-
-  return (
-    <View style={viewStyle}>
-      <Text style={textStyle}>{props.headerText}</Text>
-    </View>
-  );
-};
 
 const styles = {
   viewStyle: {
@@ -19,17 +7,24 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
-    paddingTop: 15,
+    paddingTop: 30,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
-    position: 'relative'
+    position: 'relative',
+    width: '100%'
   },
   textStyle: {
-    fontSize: 20
+    fontSize: 20,
+    color: '#146fb4'
   }
 };
 
-// Make the component available to other parts of the app
+const Header = ({ headerText }) => (
+  <View style={styles.viewStyle}>
+    <Text style={styles.textStyle}>{headerText}</Text>
+  </View>
+);
+
 export default Header;
